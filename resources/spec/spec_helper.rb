0bc3ecc3389d41d11330cbd0eb055d2e0b31d638
Tower He <towerhe@gmail.com>
2011-07-30 00:31:18 +0800

    simplecov setup

--- resources/spec/spec_helper.rb
+++ resources/spec/spec_helper.rb
@@ -1,4 +1,14 @@
-require 'rubygems'
+require 'simplecov'
+require 'simplecov-rcov'
+class SimpleCov::Formatter::MergedFormatter
+  def format(result)
+    SimpleCov::Formatter::HTMLFormatter.new.format(result)
+    SimpleCov::Formatter::RcovFormatter.new.format(result)
+  end
+end
+SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+SimpleCov.start 'rails'
+
 require 'spork'
 
 Spork.prefork do
